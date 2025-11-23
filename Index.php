59b004481d790aa65ec97d8f5c1f73bfe93737c6
hide165-design <hida165@gmail.com>
2025-11-22 21:08:53 -0500

    Comentando elementos de index

--- Index.php
+++ Index.php
@@ -10,16 +10,24 @@
           cargar_links();
     ?>
 </head>
-
+<!-- Cuerpo de la pagina, aqui de estructura todo -->
 <body>
+    <!-- menu de navegacion, al principio de la pagina -->
     <div id="Nav"><?php cargar_navegador(); ?></div>
 
+    <!-- Titulo H1 contexto principal de la pagina -->
     <div style="text-align: center;"><h1>Cootrans La Vega – Transporte seguro en Caldas con rutas y horarios actualizados</h1> </div>
 
+
+  <!-- Seccion inicial donde se presentan imagenes -->  
   <section class="contenedor">
+
+    <!-- imagenen de carretera de supia con nombre de la empresa -->
     <div class="imagen_text">
       <img src="Img/Ilustracion5.jpg" alt="carretera de entrada al municipio de supia">
       <div class="Texto">Cootrans La Vega</div>
+
+    <!-- imagen carretera de supia con mensaje llamativo -->
     </div>
     <div class="imagen-simple">
       <img src="Img/Ilustracion6.jpg" alt="carretera de salida del municipio de supia">
@@ -27,7 +35,7 @@
     </div>
   </section>
 
-
+<!-- Seccion de tarjetas con informacion basica -->
 <section class="contenido_uno">
   <h1>Servicios de transporte en Caldas: compra de tiquetes, rutas nacionales y horarios confiables</h1>
   <div class="contenedor-tarjetas">
@@ -39,12 +47,14 @@
     <div class="tarjeta">Noticias de transporte en Colombia: estado de las vías y actualizaciones de rutas en Caldas.</div>
   </div>
 
+<!-- Parrafo final con texto en diferente contenedor -->
   <div class="parrafos-finales">
     <p>Cootrans La Vega: experiencia en transporte de pasajeros en Caldas y rutas nacionales seguras.</p> 
     <p>Explora Cootrans La Vega: compra tus tiquetes en línea y consulta rutas y horarios de transporte en Caldas</p>
   </div>
 </section>
 
+    <!-- Pie de pagina -->
     <?php cargar_footer(); ?>
 </body>
 </html>
